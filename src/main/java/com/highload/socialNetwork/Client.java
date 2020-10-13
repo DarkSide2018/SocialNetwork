aa8beefae0c3c0516cd1927c54e9df45ccc26579
romangaponov <rgaponov@oboz.com>
2020-10-13 07:55:40 +0300

    add repository

--- src/main/java/com/highload/socialNetwork/Client.java
+++ src/main/java/com/highload/socialNetwork/Client.java
@@ -1,4 +1,14 @@
 package com.highload.socialNetwork;
 
+import lombok.Data;
+
+@Data
 public class Client {
+    private Long id;
+    private String name;
+    private String surName;
+    private Integer age;
+    private String gender;
+    private String interest;
+    private String city;
 }
--- src/main/java/com/highload/socialNetwork/ClientRepository.java
+++ src/main/java/com/highload/socialNetwork/ClientRepository.java
@@ -1,4 +1,33 @@
 package com.highload.socialNetwork;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+@Repository
 public class ClientRepository {
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+    @Value("${spring.datasource.username}")
+    private String user;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @PostConstruct
+    public List<Client> getAll(){
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM client");
+            boolean execute = preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            resultSet.next();
+            System.out.println("agee -> " + resultSet.getInt("age"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
 }
