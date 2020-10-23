0e921324716b2a5d029f138bbca023485ea7f1a9
romangaponov <rgaponov@oboz.com>
2020-10-23 15:21:37 +0300

    h2mem

--- src/main/java/com/highload/socialNetwork/SocialNetworkApplication.java
+++ src/main/java/com/highload/socialNetwork/SocialNetworkApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class SocialNetworkApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.default", "heroku");
+        System.setProperty("spring.profiles.default", "h2mem");
         SpringApplication.run(SocialNetworkApplication.class, args);
     }
 
