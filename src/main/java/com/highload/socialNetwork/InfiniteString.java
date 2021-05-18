604c317e4069d8e39fbba85c6a11e3419c671104
rgaponov <rgaponov@oboz.com>
2021-05-18 10:24:40 +0300

    Solution done

--- src/main/java/com/highload/socialNetwork/InfiniteString.java
+++ src/main/java/com/highload/socialNetwork/InfiniteString.java
@@ -1,19 +1,27 @@
 package com.highload.socialNetwork;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class InfiniteString {
     public static void main(String[] args) {
-            repeatedString("a",1000000000000L);
+
+        System.out.println(repeatedString("ab", 7L));
     }
 
     public static long repeatedString(String s, long n) {
         long count = s.chars().count();
-        long l = (n / count)+1;
-        StringBuilder result = new StringBuilder(s);
-        for (int i = 0; i < l; i++) {
-            result.append(s);
+        long sTimes = (n / count);
+        List<Character> characterList = s.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        long countAChar = characterList.stream().filter(el -> el.equals('a')).count();
+        long countInFullStrings = countAChar * sTimes;
+        long lengthOfFullStrings = count * sTimes;
+        long ending = n-lengthOfFullStrings;
+        for (int i = 0; i < ending; i++) {
+            if(characterList.get(i).equals('a')){
+                countInFullStrings++;
+            }
         }
-        return result.chars().mapToObj(c -> (char) c).collect(Collectors.toList()).stream().filter(el -> el.equals('a')).count();
+        return countInFullStrings;
     }
 }
