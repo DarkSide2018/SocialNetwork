5556d34919dba28f383dc5e70d9ac670c5df5b6c
rgaponov <rgaponov@oboz.com>
2021-05-20 10:16:20 +0300

    add Hour GLasses

--- src/main/java/com/highload/socialNetwork/HourGlasses.java
+++ src/main/java/com/highload/socialNetwork/HourGlasses.java
@@ -2,7 +2,9 @@ package com.highload.socialNetwork;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class HourGlasses {
     public static void main(String[] args) {
@@ -14,16 +16,33 @@ public class HourGlasses {
     public static int hourglassSum(List<List<Integer>> arr) {
         System.out.println(arr);
         arr.forEach(System.out::println);
-
+        convertTOGlasses(arr);
         return 7;
     }
-    public static List<List<Integer>> convertTOGlasses(List<List<Integer>> arr){
-        arr.forEach(el->
-                {
-                    List<Integer> integers = el.subList(0, 2);
 
+    public static List<List<Integer>> convertTOGlasses(List<List<Integer>> arr) {
+        int arrSize = arr.size();
+        List<List<Integer>> glasses = new ArrayList<>();
+        IntStream.range(0, arrSize).forEach(index -> {
+            List<Integer> el = arr.get(index);
+            System.out.println("index -> " + index);
+            int i1 = index % 2;
+            System.out.println("ostatok -> " + i1);
+            if (i1 != 0) {
+                for (int i = 1; i < el.size() - 1; i++) {
+                    glasses.add(Arrays.asList(0, el.get(i), 0));
                 }
-                );
+            } else {
+                System.out.println("i1 == 0");
+                for (int i = 0; i < el.size() - 2; i++) {
+                    List<Integer> integers = el.subList(i, i + 3);
+                    glasses.add(integers);
+                }
+            }
+            System.out.println("current glasses -> " + glasses);
+        });
+        glasses.forEach(System.out::println);
+        System.out.println("---------------------");
         return new ArrayList<>();
     }
 }
